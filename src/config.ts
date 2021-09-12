1c1492ad55289d1c262fd38aeb15ea21760cd50a
Dan Bennett <dghbennett47@gmail.com>
2021-09-12 23:31:29 +0100

    feat: create config file

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+const proxyURL = 'https://thawing-ocean-11321.herokuapp.com';
+
+export const url = {
+	general: `${proxyURL}/fantasy.premierleague.com/api/bootstrap-static/`,
+	players: `${proxyURL}/fantasy.premierleague.com/api/element-summary/`, // addd element_id after the '/'
+};
