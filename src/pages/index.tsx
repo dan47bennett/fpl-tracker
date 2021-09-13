dcb38bb43f81bad5521a85bd49ae1053cdbe40ac
Dan Bennett <dghbennett47@gmail.com>
2021-09-14 00:42:55 +0100

    feat: load all players and rank by total points

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,39 @@
 import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+import { getAllPlayerInfo } from '../api';
+import { Player } from '../types';
 
 const Index: NextPage = () => {
-	return <div>FPL Tracker</div>;
+	const [allData, setAllData] = useState<Player[]>([]);
+	const [displayData, setDisplayData] = useState<Player[]>([]);
+
+	const loadData = async () => {
+		const info = await getAllPlayerInfo();
+		setAllData(info);
+	};
+	useEffect(() => {
+		loadData();
+	}, []);
+
+	useEffect(() => {
+		const sortedByGoals = allData.sort((a, b) => {
+			if (a.total_points < b.total_points) return 1;
+			if (a.total_points > b.total_points) return -1;
+			return 0;
+		});
+		console.log('sortedByGoals: ', sortedByGoals);
+		setDisplayData(sortedByGoals);
+	}, [allData]);
+
+	return (
+		<>
+			<div>FPL Tracker</div>
+
+			{displayData.map((player) => (
+				<div key={player.id}>{player.second_name}</div>
+			))}
+		</>
+	);
 };
 
 export default Index;
