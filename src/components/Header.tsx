a2f5b8ddc13624a0d7563573c522a651cccb35e3
Dan Bennett <dghbennett47@gmail.com>
2021-09-15 00:42:19 +0100

    feat: add header wrap around site

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderDiv = styled.div`
+	background-color: ${({ theme }) => theme.colors.secondary};
+	color: ${({ theme }) => theme.colors.primary};
+	height: 80px;
+	padding-left: 30px;
+	display: flex;
+	align-items: center;
+	text-align: center;
+`;
+
+export const Header: React.FC = () => {
+	return <HeaderDiv>FPL Tracker</HeaderDiv>;
+};
