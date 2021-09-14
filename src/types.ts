d30dc402138b3871d8de8ed990056025b5acae46
Dan Bennett <dghbennett47@gmail.com>
2021-09-14 16:51:55 +0100

    chore: separate sortable props

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,36 @@
-export type Player = {
+export interface PlayerSortProps {
 	assists: number;
 	bonus: number;
 	bps: number;
+	clean_sheets: number;
+	event_points: number;
+	form: string;
+	goals_conceded: number;
+	goals_scored: number;
+	id: number;
+	minutes: number;
+	now_cost: number;
+	own_goals: number;
+	penalties_missed: number;
+	penalties_saved: number;
+	points_per_game: string;
+	red_cards: number;
+	saves: number;
+	second_name: string;
+	selected_by_percent: string;
+	total_points: number;
+	transfers_in: number;
+	transfers_in_event: number;
+	transfers_out: number;
+	transfers_out_event: number;
+	value_form: string;
+	value_season: string;
+	yellow_cards: number;
+}
+
+export interface Player extends PlayerSortProps {
 	chance_of_playing_next_round: number | null;
 	chance_of_playing_this_round: number | null;
-	clean_sheets: number;
 	code: number;
 	corners_and_indirect_freekicks_order: number | null;
 	corners_and_indirect_freekicks_text: string;
@@ -21,34 +47,19 @@ export type Player = {
 	element_type: number;
 	ep_next: string;
 	ep_this: string;
-	event_points: number;
 	first_name: string;
-	form: string;
-	goals_conceded: number;
-	goals_scored: number;
 	ict_index: string;
 	ict_index_rank: number;
 	ict_index_rank_type: number;
-	id: number;
 	in_dreamteam: boolean;
 	influence: string;
 	influence_rank: number;
 	influence_rank_type: number;
-	minutes: number;
 	news: string;
 	news_added: number | null;
-	now_cost: number;
-	own_goals: number;
-	penalties_missed: number;
 	penalties_order: number | null;
-	penalties_saved: number;
 	penalties_text: string;
 	photo: string;
-	points_per_game: string;
-	red_cards: number;
-	saves: number;
-	second_name: string;
-	selected_by_percent: string;
 	special: boolean;
 	squad_number: number | null;
 	status: string;
@@ -57,13 +68,5 @@ export type Player = {
 	threat: string;
 	threat_rank: number;
 	threat_rank_type: number;
-	total_points: number;
-	transfers_in: number;
-	transfers_in_event: number;
-	transfers_out: number;
-	transfers_out_event: number;
-	value_form: string;
-	value_season: string;
 	web_name: string;
-	yellow_cards: number;
-};
+}
