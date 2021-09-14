87ec3f567b79b4ab8f2fff3a8744649a637e9732
Dan Bennett <dghbennett47@gmail.com>
2021-09-15 00:42:42 +0100

    feat: add theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,40 @@
 import type { AppProps } from 'next/app';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { Layout } from '../components/Layout';
+
+const GlobalStyle = createGlobalStyle`
+html{
+  background: #F0F9FA;
+  display:block;
+  height: 100%;
+  width:100%;
+}
+
+body{
+  min-height:100vh;
+  font-family:Verdana;
+  margin: 0 auto
+}
+`;
+
+const theme = {
+	colors: {
+		primary: '#F0F9FA',
+		secondary: '#46A2C3',
+		alt: '#093249',
+	},
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ThemeProvider>
+		</>
+	);
 }
 export default MyApp;
