dfd86311ebb6fa26be5cab814452656ba778a71a
Dan Bennett <dghbennett47@gmail.com>
2021-09-16 00:46:11 +0100

    feat: move everything except api calls from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,22 @@
 import type { NextPage } from 'next';
 import React, { useEffect, useState } from 'react';
 import { getAllPlayerInfo } from '../api';
-import { PlayerEntry } from '../components/PlayerEntry';
-import { RankingTable } from '../components/RankingTable';
+import { RankingTable } from '../components/ranking-table/RankingTable';
 import { Player } from '../types';
-import { sortByKey } from '../utils/sortByKey';
 
 const Index: NextPage = () => {
 	const [allData, setAllData] = useState<Player[]>([]);
-	const [displayData, setDisplayData] = useState<Player[]>([]);
 
 	const loadData = async () => {
 		const info = await getAllPlayerInfo();
 		setAllData(info);
 	};
+
 	useEffect(() => {
 		loadData();
 	}, []);
 
-	useEffect(() => {
-		const sortedArray = sortByKey(allData, 'total_points');
-		console.log('sortedArray: ', sortedArray);
-		setDisplayData(sortedArray);
-	}, [allData]);
-
-	return <RankingTable players={displayData} />;
+	return <RankingTable players={allData} />;
 };
 
 export default Index;
