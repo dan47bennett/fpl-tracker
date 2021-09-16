076b856a9acb793df61df96542314659dcdf39db
Dan Bennett <dghbennett47@gmail.com>
2021-09-16 01:46:44 +0100

    chore: move container styles

--- src/components/ranking-table/PlayerTable.tsx
+++ src/components/ranking-table/PlayerTable.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { Player } from '../types';
-import styled from 'styled-components';
+import { Player } from '../../types';
 import { PlayerEntry } from './PlayerEntry';
-
-const TableContainer = styled.div`
-	margin: 20px auto;
-	width: 90%;
-	font-size: ${({ theme }) => theme.fontSize.table};
-`;
 
 export const PlayerTable: React.FC<{ players: Player[] }> = ({ players }) => {
 	return (
-		<TableContainer>
+		<>
 			{players.length > 0 ? (
 				players.map((player) => (
 					<PlayerEntry key={player.id} player={player} />
@@ -19,6 +12,6 @@ export const PlayerTable: React.FC<{ players: Player[] }> = ({ players }) => {
 			) : (
 				<div>Loading...</div>
 			)}
-		</TableContainer>
+		</>
 	);
 };
