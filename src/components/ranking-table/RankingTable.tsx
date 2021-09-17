dd0844cb1b6b01c451c81697702c6c5aa41abed9
Dan Bennett <dghbennett47@gmail.com>
2021-09-17 02:13:03 +0100

    feat: reverse sort by clicking heading twice

--- src/components/ranking-table/RankingTable.tsx
+++ src/components/ranking-table/RankingTable.tsx
@@ -1,29 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { TableContainer } from '../../styles/playerTableStyles';
-import { Player, PlayerSortProps } from '../../types';
+import { Player, playerProp, PlayerSortProps } from '../../types';
 import { sortByKey } from '../../utils/sortByKey';
 import { PageSelectionButtons } from './PageSelectionButtons';
 import { PlayerTable } from './PlayerTable';
 import { TableHeadings } from './TableHeadings';
 
 export const RankingTable: React.FC<{ players: Player[] }> = ({ players }) => {
-	const [sortingKey, setSortingKey] = useState<keyof PlayerSortProps>(
-		'total_points'
-	);
+	const [sortingKey, setSortingKey] = useState<playerProp>('total_points');
 	const [displayData, setDisplayData] = useState<Player[]>([]);
 	const [page, setPage] = useState(0);
+	const [reverse, setReverse] = useState(false);
 
 	useEffect(() => {
 		if (players) {
-			const sortedArray = sortByKey(players, sortingKey);
+			const sortedArray = sortByKey(players, sortingKey, reverse);
 			const slicedData = sortedArray.slice(20 * page, 20 * (page + 1));
 			setDisplayData(slicedData);
 		}
-	}, [players, sortingKey, page]);
+	}, [players, sortingKey, page, reverse]);
+
+	const onHeadingClick = (sortProp: playerProp) => {
+		if (sortProp === sortingKey) {
+			setReverse(!reverse);
+		} else {
+			setReverse(false);
+		}
+		setPage(0);
+		setSortingKey(sortProp);
+	};
 
 	return (
 		<TableContainer>
-			<TableHeadings setSortingKey={setSortingKey} setPage={setPage} />
+			<TableHeadings
+				onHeadingClick={onHeadingClick}
+				sortingKey={sortingKey}
+			/>
 			<PlayerTable players={displayData} />
 			<PageSelectionButtons
 				onClickPrev={() => {
