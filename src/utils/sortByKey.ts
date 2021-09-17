a5ca4ecaa1e9fe517be98fa0537776c0de0233c3
Dan Bennett <dghbennett47@gmail.com>
2021-09-17 02:13:25 +0100

    feat: add reverse sort

--- src/utils/sortByKey.ts
+++ src/utils/sortByKey.ts
@@ -1,6 +1,19 @@
-import { Player, PlayerSortProps } from '../types';
+import { Player, playerProp } from '../types';
 
-export const sortByKey = (array: Player[], key: keyof PlayerSortProps) => {
+export const sortByKey = (
+	array: Player[],
+	key: playerProp,
+	reverse: boolean
+) => {
+	if (key === 'web_name') reverse = !reverse;
+
+	if (reverse) {
+		return array.sort((a, b) => {
+			if (a[key] < b[key]) return -1;
+			if (a[key] > b[key]) return 1;
+			return 0;
+		});
+	}
 	return array.sort((a, b) => {
 		if (a[key] < b[key]) return 1;
 		if (a[key] > b[key]) return -1;
