ac96eada78ddfbee48b1ccbfa140b17476c7142c
Dan Bennett <dghbennett47@gmail.com>
2021-09-29 00:02:05 +0100

    fix: types in sortByKey

--- src/utils/sortByKey.ts
+++ src/utils/sortByKey.ts
@@ -1,5 +1,9 @@
 import { Player, playerProp } from '../types';
 
+const helpType = (value: any) => {
+	return value as number | string | number;
+};
+
 export const sortByKey = (
 	array: Player[],
 	key: playerProp,
@@ -8,15 +12,23 @@ export const sortByKey = (
 	if (key === 'web_name') reverse = !reverse;
 
 	if (reverse) {
-		return array.sort((a, b) => {
-			if (a[key] < b[key]) return -1;
-			if (a[key] > b[key]) return 1;
+		return array.sort((a: Player, b: Player) => {
+			if (helpType(a[key]) < helpType(b[key])) {
+				return -1;
+			}
+			if (helpType(a[key]) > helpType(b[key])) {
+				return 1;
+			}
 			return 0;
 		});
 	}
-	return array.sort((a, b) => {
-		if (a[key] < b[key]) return 1;
-		if (a[key] > b[key]) return -1;
+	return array.sort((a: Player, b: Player) => {
+		if (helpType(a[key]) < helpType(b[key])) {
+			return 1;
+		}
+		if (helpType(a[key]) > helpType(b[key])) {
+			return -1;
+		}
 		return 0;
 	});
 };
