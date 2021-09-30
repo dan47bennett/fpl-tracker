c50d00925ae84b6726ac340b962da4a9ca18a0c4
Dan Bennett <dghbennett47@gmail.com>
2021-10-01 00:56:40 +0100

    fix: module import path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import React, { useEffect, useState } from 'react';
 import { getAllPlayerInfo } from '../api';
-import { RankingTable } from '../components/ranking-table/RankingTable';
+import { RankingTable } from '../components/home/ranking-table/RankingTable';
 import { Player } from '../types';
 
 const Index: NextPage = () => {
