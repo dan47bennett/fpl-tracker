4bbe6dfddd644cad38bc6f703eab3aba38da9b83
Dan Bennett <dghbennett47@gmail.com>
2021-10-02 01:17:19 +0100

    chore: define width for mobile-desktop switch

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 const proxyURL = 'https://thawing-ocean-11321.herokuapp.com';
 
+export const maxMobileWidth = 750;
+
 export const url = {
 	general: `${proxyURL}/fantasy.premierleague.com/api/bootstrap-static/`,
 	players: `${proxyURL}/fantasy.premierleague.com/api/element-summary/`, // addd element_id after the '/'
