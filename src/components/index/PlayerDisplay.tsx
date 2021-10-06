f2f229b45b41162c6ee7dd63ad613dfffc5dd68d
Dan Bennett <dghbennett47@gmail.com>
2021-10-06 01:56:57 +0100

    feat: text and styling

--- src/components/index/PlayerDisplay.tsx
+++ src/components/index/PlayerDisplay.tsx
@@ -3,6 +3,7 @@ import { Player } from '../../types';
 import { RankingTable } from './ranking-table/RankingTable';
 import { teamAbbrs } from '../../config';
 import { SortSelect } from '../../styles/playerTableStyles';
+import { ContainerHorizontalResp, TextBox } from '../../styles/genericStyles';
 
 interface PlayerDisplayProps {
 	players: Player[];
@@ -50,30 +51,38 @@ export const PlayerDisplay: React.FC<PlayerDisplayProps> = ({ players }) => {
 
 	return (
 		<>
-			<label htmlFor="team-select">Sort by team:</label>
-			<SortSelect id="team-select" onChange={onChangeTeam}>
-				<option value={'all'}>All</option>
-				{allNames.map((name) => {
-					const index = allNames.indexOf(name);
-					return (
-						<option key={index} value={allAbbrs[index]}>
-							{name}
-						</option>
-					);
-				})}
-			</SortSelect>
-			<label htmlFor="position-select">Sort by position:</label>
-			<SortSelect id="position-select" onChange={onChangePosition}>
-				<option value={'all'}>All</option>
-				{positionInitials.map((position) => {
-					const index = positionInitials.indexOf(position);
-					return (
-						<option key={index} value={positionInitials[index]}>
-							{positionWords[index]}
-						</option>
-					);
-				})}
-			</SortSelect>
+			<TextBox>
+				Welcome to FPL Tracker. Here you can get a more in-depth insight
+				into the profile and stats behind your current stars and
+				potential transfer targets.
+			</TextBox>
+			<ContainerHorizontalResp>
+				<label htmlFor="team-select">Team</label>
+				<SortSelect id="team-select" onChange={onChangeTeam}>
+					<option value={'all'}>All</option>
+					{allNames.map((name) => {
+						const index = allNames.indexOf(name);
+						return (
+							<option key={index} value={allAbbrs[index]}>
+								{name}
+							</option>
+						);
+					})}
+				</SortSelect>
+				<label htmlFor="position-select">Position</label>
+				<SortSelect id="position-select" onChange={onChangePosition}>
+					<option value={'all'}>All</option>
+					{positionInitials.map((position) => {
+						const index = positionInitials.indexOf(position);
+						return (
+							<option key={index} value={positionInitials[index]}>
+								{positionWords[index]}
+							</option>
+						);
+					})}
+				</SortSelect>
+			</ContainerHorizontalResp>
+
 			<RankingTable players={displayData} page={page} setPage={setPage} />
 		</>
 	);
---
+++ src/styles/genericStyles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+`;
+
+export const ContainerHorizontalResp = styled.div`
+	width: 35%;
+	display: flex;
+	flex-direction: row;
+	@media only screen and (max-width: 700px) {
+		flex-direction: column;
+	}
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+`;
+
+export const TitleBox = styled.div`
+	font-size: ${({ theme }) => theme.fontSize.title};
+	width: 60%;
+	margin: 10px auto;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+`;
+
+export const TextBox = styled.div`
+	font-size: ${({ theme }) => theme.fontSize.subTitle};
+	width: 60%;
+	margin: 10px auto;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+`;
